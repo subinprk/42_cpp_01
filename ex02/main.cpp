8a2f8752525b651ca6252594e82193ae92b31c8c
subinprk <subpark@c4b10c3.42berlin.de>
2024-03-12 19:07:20 +0100

    b4 going home, have to check difference btw reference, and pointer

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -5,8 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: subpark <subpark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/12 19:03:02 by subpark           #+#    #+#             */
-/*   Updated: 2024/03/12 19:05:40 by subpark          ###   ########.fr       */
+/*   Created: 2024/03/12 19:04:23 by subpark           #+#    #+#             */
+/*   Updated: 2024/03/12 19:07:14 by subpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string.h>
+
+int main()
+{
+    std::string str("HI THIS IS BRAIN");
+
+    std::string *stringPTR;
+    std::string &stringREF;
+    
+}
